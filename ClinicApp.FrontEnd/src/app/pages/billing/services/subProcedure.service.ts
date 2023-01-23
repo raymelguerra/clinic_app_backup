303b20e4c9daa29a4386d3df76e47840566c585a
raymelguerra <raymelguerra@gmail.com>
2023-01-23 13:08:11 -0500

    fix error in subprocedure

--- ClinicApp.FrontEnd/src/app/pages/billing/services/subProcedure.service.ts
+++ ClinicApp.FrontEnd/src/app/pages/billing/services/subProcedure.service.ts
@@ -10,7 +10,7 @@ import { SubProcedure } from '../models/subProcedure.model';
 })
 export class SubProcedureService {
 
-  private subProcedureUrl = environment.apiUrl + 'subProcedure';
+  private subProcedureUrl = environment.apiUrl + 'infrastructure/subProcedure';
   // private subProcedureUrl = 'http://localhost:5233/api/infrastructure/' + 'subProcedure';
 
   constructor(private http: HttpClient) { }
