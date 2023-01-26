0d8a9293123bfe58c44c37b7b24b20076f618497
raymelguerra <raymelguerra@gmail.com>
2023-01-26 00:21:34 -0500

    fix test env

--- ClinicApp.FrontEnd/src/environments/environment.prod.ts
+++ ClinicApp.FrontEnd/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://migration.abaanalystgroup.live/api/', // Development
+  apiUrl: 'http://clinicapp.backend.proxy/api/', // Development
   // apiUrl: 'http://34.228.60.99:8082/api/', // Expanding y VillaLyan
   // apiUrl: 'http://34.228.60.99:8088/api/', // Principles of learning
   // apiUrl: 'http://34.228.60.99:8084/api/', // Test
