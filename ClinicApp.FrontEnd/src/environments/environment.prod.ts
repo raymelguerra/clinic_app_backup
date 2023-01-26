8a07e45f4e269ceed9aa723fbe0496a4d56e0bc6
raymelguerra <raymelguerra@gmail.com>
2023-01-26 00:32:52 -0500

    Hardcoded url in frontend angular

--- ClinicApp.FrontEnd/src/environments/environment.prod.ts
+++ ClinicApp.FrontEnd/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://clinicapp.backend.proxy/api/', // Development
+  apiUrl: 'https://test.abaanalystgroup.live/api/', // Development
   // apiUrl: 'http://34.228.60.99:8082/api/', // Expanding y VillaLyan
   // apiUrl: 'http://34.228.60.99:8088/api/', // Principles of learning
   // apiUrl: 'http://34.228.60.99:8084/api/', // Test
