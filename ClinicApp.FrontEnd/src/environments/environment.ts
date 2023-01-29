bd8a7649222881d8a0ca73d7351304db69ec9772
raymelguerra <raymelguerra@gmail.com>
2023-01-29 03:12:31 -0500

    principles deploy

--- ClinicApp.FrontEnd/src/environments/environment.ts
+++ ClinicApp.FrontEnd/src/environments/environment.ts
@@ -5,10 +5,5 @@
 
 export const environment = {
   production: true,
-  apiUrl: 'https://test.abaanalystgroup.live/api/', // Development
-  // apiUrl: 'https://migration.abaanalystgroup.live/api/', // Development
-  // apiUrl: 'http://34.224.222.1:8082/api/', // Expanding y VillaLyan
-  // apiUrl: 'http://34.224.222.1:8088/api/', // Principles of learning
-  // apiUrl: 'http://34.224.222.1:8084/api/', // Test
-  // apiUrl: 'http://34.224.222.1:8083/api/', // Brain & behavioral institute
+  apiUrl: 'https://principles.abaanalystgroup.live/api/'
 };
