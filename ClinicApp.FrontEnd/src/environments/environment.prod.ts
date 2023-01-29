abec482ce6a34a7dbedda82ce1ce3c5b3566b0aa
raymelguerra <raymelguerra@gmail.com>
2023-01-29 02:38:52 -0500

    fix env in front

--- ClinicApp.FrontEnd/src/environments/environment.prod.ts
+++ ClinicApp.FrontEnd/src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://test.abaanalystgroup.live/api/', // Development
-  // apiUrl: 'http://34.228.60.99:8082/api/', // Expanding y VillaLyan
-  // apiUrl: 'http://34.228.60.99:8088/api/', // Principles of learning
-  // apiUrl: 'http://34.228.60.99:8084/api/', // Test
-  // apiUrl: 'http://34.228.60.99:8083/api/', // Brain & behavioral institute
+  apiUrl: 'https://brain.abaanalystgroup.live/api/'
 };
